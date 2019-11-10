c506e4c8cedf710eec3ef9ff73cff316e9b522fe
sahinakyol <akyolsahinn@gmail.com>
2019-11-10 09:44:19 +0300

    Project initialized

---
+++ src/main/java/com/example/kloia/KloiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kloia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KloiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KloiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kloia/KloiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kloia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KloiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
